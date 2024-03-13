5368d38d476b18c77039371bfaec6ddde7708f9c
plague-boy <tommmysmith.204@gmail.com>
2024-03-14 10:03:13 +1300

    forgor to push

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 ## MDDN 242 Project 1: Time-based Media  
 
 10/02/2024
-My idea is inspired by the flight boards seen in airports, I plan on having a middle strip in the desig with an aeroplane traveling with the hours, flying from sunrise to sunset with an ombre that changes with the hour, and the plane traveling from hours 1-12, with an alarm at hour 6, 12, and 18.
+My idea is inspired by the flight boards seen in airports, I plan on having a middle strip in the desig with an aeroplane traveling with the hours, flying from sunrise to sunset with an ombre that changes with the hour, and the plane traveling from hours 1-12, the alarm will feature a bird hitting the windshield of the plane and being wiped off.
+
+12/02/2024
+I decided to spend today trying to get the lerp colour gradient to work in conjunction with time. First I had to learn how to make a solid gradient, for which I used https://www.youtube.com/watch?v=lPgscmgxcH0 to learn from, then I attempted to evolve it from their to include for if-else requests around the changes in time. To start of with I decided to work in seconds so that I could see the changes more clearly
--- clock.js
+++ clock.js
@@ -12,17 +12,52 @@ function draw_clock(obj) {
   //        = 0 if the alarm is currently going off
   //        > 0 --> the number of seconds until alarm should go off
   background(50); //  beige
-  fill(200); // dark grey
-  textSize(40);
-  textAlign(CENTER, CENTER);
-  text("YOUR MAIN CLOCK CODE GOES HERE", width / 2, 200);
+  // fill(200); // dark grey
+  let risesky = color(255, 141, 68);
+  let daysky = color(89, 185, 255);
+  let setsky = color(60, 3, 87);
+  let nightsky = color (1, 8, 64);
+  let sunriselerp = lerpColor(risesky,daysky,obj.seconds/59)
+  // let sunrise = lerpColor(risesky, daysky, amt);
+  // let sunset = lerpColor(daysky, setsky, amt );
+  // let nighttime = lerpColor(setsky, nightsky, amt );
+  //let twilight = lerpColor (nightsky, risesky,amt);
 
+  // textSize(40);
+  // textAlign(CENTER, CENTER);
+  // text("YOUR MAIN CLOCK CODE GOES HERE", width / 2, 200);
+  
+//sky
+// Twilight runs from 6:00 to 6:15
+// Sunrise runs from 6:15 to 7:00
+// Daylight runs from 7:00 to 18:00
+// Sunset runs from 18:00 to 18:45
+// Set to Night runs from 18:45 to 19:00
+// Night runs from 19:00 to 6:00
+fill(sunriselerp)
+rect(0,0,960,500)
 
-  fill(249, 140, 255);// pink
-  ellipse(width / 3, 350, 150);
-  fill(140, 255, 251) // blue
-  ellipse(width / 2, 350, 150);
-  fill(175, 133, 255); // purple
-  ellipse(width / 3 * 2, 350, 150);
+//clocks
+let clockx = 40; //X cord for the clock
+let clocky = 40; //Y cord for the clock
+let clockw = 90; //Width of the clock
+let clockh = 100; //Height of the clock
+let clockcurve = 20; //Curve of the clock
+//Hour
+  fill(0);
+  rect(clockx,clocky,clockw,clockh,clockcurve);
+  rect(clockx + 95,clocky,clockw,clockh,clockcurve);
+
+//Minute
+  rect(clockx + 348,clocky,clockw,clockh,clockcurve);
+  rect(clockx + 443,clocky,clockw,clockh,clockcurve);
+
+//Second
+  rect(clockx + 691,clocky,clockw,clockh,clockcurve);
+  rect(clockx + 786,clocky,clockw,clockh,clockcurve);
+
+// stroke(0);
+// line(480, 0, 480, 500);
+
 
 }
