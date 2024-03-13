e013689a920debc52d02d1b898190fc04db41a70
plague-boy <tommmysmith.204@gmail.com>
2024-03-14 10:33:02 +1300

    Update clock.js

--- clock.js
+++ clock.js
@@ -11,31 +11,35 @@ function draw_clock(obj) {
   //        < 0 if no alarm is set
   //        = 0 if the alarm is currently going off
   //        > 0 --> the number of seconds until alarm should go off
-  background(50); //  beige
+  background(1000); //  beige
   // fill(200); // dark grey
   let risesky = color(255, 141, 68);
   let daysky = color(89, 185, 255);
   let setsky = color(60, 3, 87);
   let nightsky = color (1, 8, 64);
-  let sunriselerp = lerpColor(risesky,daysky,obj.seconds/59)
-  // let sunrise = lerpColor(risesky, daysky, amt);
-  // let sunset = lerpColor(daysky, setsky, amt );
-  // let nighttime = lerpColor(setsky, nightsky, amt );
-  //let twilight = lerpColor (nightsky, risesky,amt);
-
-  // textSize(40);
-  // textAlign(CENTER, CENTER);
-  // text("YOUR MAIN CLOCK CODE GOES HERE", width / 2, 200);
   
+
 //sky
-// Twilight runs from 6:00 to 6:15
-// Sunrise runs from 6:15 to 7:00
-// Daylight runs from 7:00 to 18:00
-// Sunset runs from 18:00 to 18:45
-// Set to Night runs from 18:45 to 19:00
-// Night runs from 19:00 to 6:00
-fill(sunriselerp)
-rect(0,0,960,500)
+    // Twilight runs from 6:00 to 6:15
+    // Sunrise runs from 6:15 to 7:00
+    // Daylight runs from 7:00 to 18:00
+    // Sunset runs from 18:00 to 18:45
+    // Set to Night runs from 18:45 to 19:00
+    // Night runs from 19:00 to 6:00
+    // fill(sunriselerp)
+    // rect(0,0,960,500)
+let myStrokeWeight = 50;
+strokeWeight(myStrokeWeight);
+for(let i=1; i<width; i = i + myStrokeWeight+1){
+  let lerpMAP = map(i, 0, width, 0, obj.seconds/59*5);
+  let sunriselerp = lerpColor(risesky,daysky,lerpMAP)
+  stroke(sunriselerp)
+  line(i,0,i,height);
+  i/50
+
+}
+
+
 
 //clocks
 let clockx = 40; //X cord for the clock
@@ -44,11 +48,17 @@ let clockw = 90; //Width of the clock
 let clockh = 100; //Height of the clock
 let clockcurve = 20; //Curve of the clock
 //Hour
+  noStroke()
   fill(0);
   rect(clockx,clocky,clockw,clockh,clockcurve);
   rect(clockx + 95,clocky,clockw,clockh,clockcurve);
+  stroke(10);
+  textSize(40);
+  textAlign(CENTER, CENTER);
+  text(obj.hours);
 
 //Minute
+  noStroke()
   rect(clockx + 348,clocky,clockw,clockh,clockcurve);
   rect(clockx + 443,clocky,clockw,clockh,clockcurve);
 
@@ -56,8 +66,6 @@ let clockcurve = 20; //Curve of the clock
   rect(clockx + 691,clocky,clockw,clockh,clockcurve);
   rect(clockx + 786,clocky,clockw,clockh,clockcurve);
 
-// stroke(0);
-// line(480, 0, 480, 500);
 
 
 }
